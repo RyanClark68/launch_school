81ebfdb9d6f7a5cccac4e371f23827fe679f3eaf
RyanClark68 <beckyclark2@me.com>
2016-03-13 22:01:47 -0400

    Create array.rb

---
+++ array.rb
@@ -0,0 +1,10 @@
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+arr.each do |num|
+  if num == number
+    puts "#{number} is in the array."
+  end
+end
+
+arr = [["test", "hello", "world"], "example", "mem"]]
+puts arr.last.first
